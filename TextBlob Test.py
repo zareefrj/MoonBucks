db866782878d2d00d0c23ab82d6ef22a9f64a589
zareefrj <17207394@siswa.um.edu.my>
2022-05-23 21:47:56 +0800

    Plotly

--- TextBlob Test.py
+++ TextBlob Test.py
@@ -1,6 +1,6 @@
 #YOU CAN USE THIS FILE TO EXPERIMENT ON TEXTBLOB or other stuff :)
 #READING THE TXT FILE
-with open ("testtext.txt","r",encoding="utf-8") as file:
+"""with open ("testtext.txt","r",encoding="utf-8") as file:
     var=file.read().lower() #Lowercase everything
     #print(var)
     file.close()
@@ -15,3 +15,17 @@ punctuation= '''!()-[]{};:'"\, <>./?@#$%^&*_~'''
 for x in punctuation:
     var=var.replace(x,"")
 print(var)
+"""
+#PLOTLY
+#$ pip install plotly==5.8.0
+#try scatter or line
+import plotly.graph_objects as go
+fig = go.Figure(
+        data=[go.Bar(x=["kl"], y=[1])],
+        layout=go.Layout(
+            title=go.layout.Title(text="The Sentiment of Countries")
+        )
+    )
+
+fig.show()
+#for i in range(5): it creates individual graphs
